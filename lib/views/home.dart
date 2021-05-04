7397200d45676a62c06c2949140752c262c35e57
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-05-04 10:27:34 +0100

    began working on home page layout

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -12,20 +12,42 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text('Messenger Clone'),
-      actions: [
-        InkWell(
-          onTap: () {
-            AuthMethods().signOut().then((s) {
-              Navigator.pushReplacement(
-                  context, MaterialPageRoute(builder: (context) => SignIn()));
-            });
-          },
-          child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Icon(Icons.exit_to_app)),
-        )
-      ],
-    ));
+          title: Text('Messenger Clone'),
+          actions: [
+            InkWell(
+              onTap: () {
+                AuthMethods().signOut().then((s) {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SignIn()));
+                });
+              },
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Icon(Icons.exit_to_app)),
+            )
+          ],
+        ),
+        body: Container(
+            child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.black87,
+                      width: 1.0,
+                      style: BorderStyle.solid)),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Username"),
+                  )),
+                  Icon(Icons.search)
+                ],
+              ),
+            )
+          ],
+        )));
   }
 }
